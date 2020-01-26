7a971b37809734869027404af423e069737c13be
Jon Waterman <JonRWaterman@gmail.com>
2020-01-25 19:15:03 -0600

    function headers added

--- LUIS-API-Calls/add_intent.js
+++ LUIS-API-Calls/add_intent.js
@@ -1,13 +1,19 @@
+//  Jon Waterman
+//  purpose: adds an intent to the app
+//  inputs: name of intent
+//  outputs: ID of new query (I'm fairly sure)
+
 var request = require("request");
 
 var options = { method: 'POST',
   url: 'https://{endpoint_url}/luis/api/v2.0/apps/{appID}/versions/{versionID}/intents',
-  qs: { 'subscription-key': '7ff2eb676c8640cf9416e0f7229f9f32' },
+  //  switch out endpoint_url, versionID, and appID
+  qs: { 'subscription-key': '<subscription_key>' }, // fill in
   headers: 
    { 'Postman-Token': 'be00f55c-c71a-42c1-9430-27e123d72cf7',
      'cache-control': 'no-cache',
      'Content-Type': 'application/json' },
-  body: { name: '<new_intent_name>' },
+  body: { name: '<new_intent_name>' },  // fill in
   json: true };
 
 request(options, function (error, response, body) {
