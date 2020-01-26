0a3b423d8337b34130d1d2529bb8de242b25ca03
Jon Waterman <JonRWaterman@gmail.com>
2020-01-25 21:55:20 -0600

    rearranges files

--- LUIS-API-Calls/add_app.js
+++ LUIS-API-Calls/add_app.js
@@ -1,26 +1,31 @@
 //  Jon Waterman
-//  purpose: adds an intent to the app
-//  inputs: name of intent
-//  outputs: ID of new query
+//  Inputs: Name and description of application
+//  Outputs: appID
 
 var request = require("request");
 var readlineSync = require('readline-sync');
 
-var intent_name = readlineSync.question('Intent Name: ') 
+var app_name = readlineSync.question('App Name: ')
+var app_description = readlineSync.question('App Description: ') 
 
 const endpoint = "th2020-practice.cognitiveservices.azure.com"
-const appID = "dc41dd7b-dfdc-4ab4-b196-9776f536bfab"
-const versionID = "0.1"
 const subscription_key = "7ff2eb676c8640cf9416e0f7229f9f32"
 
 var options = { method: 'POST',
-  url: 'https://' + endpoint + '/luis/api/v2.0/apps/'+ appID + '/versions/' + versionID +'/intents',
+  url: 'https://' + endpoint + '/luis/api/v2.0/apps/',
   qs: { 'subscription-key': subscription_key },
   headers: 
-   { 'Postman-Token': 'be00f55c-c71a-42c1-9430-27e123d72cf7',
+   { 'Postman-Token': 'e7346b89-bdc4-497f-afd8-382ea871b85d',
      'cache-control': 'no-cache',
      'Content-Type': 'application/json' },
-  body: { name: intent_name },
+  body: 
+   { name: app_name,
+     description: app_description,
+     culture: 'en-us',
+     tokenizerVersion: '1.0.0',
+     usageScenario: 'IoT',
+     domain: 'Comics',
+     initialVersionId: '1.0' },
   json: true };
 
 request(options, function (error, response, body) {
