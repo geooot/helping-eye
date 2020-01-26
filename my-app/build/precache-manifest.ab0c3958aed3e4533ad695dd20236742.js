c32dc4ab506a273a9059835189b41006583a2fb6
Anikait Sharma <anikaitsharma@yahoo.com>
2020-01-26 04:06:13 -0600

    initial user interface

---
+++ my-app/build/precache-manifest.ab0c3958aed3e4533ad695dd20236742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "674ba111bce8d8d569d1cb425f3ae1bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "635e8d0e5ebe91e6da25",
+    "url": "/static/css/main.58d1d0f8.chunk.css"
+  },
+  {
+    "revision": "be8570c295897239f644",
+    "url": "/static/js/2.86ee2f15.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.86ee2f15.chunk.js.LICENSE"
+  },
+  {
+    "revision": "635e8d0e5ebe91e6da25",
+    "url": "/static/js/main.d0be6a67.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import {SidePanel} from './components/sidePanel.js'
+
+class App extends Component {
+  render() {
+    return (
+      <SidePanel/> 
+    );
+  }
+}
+
+export default App;
